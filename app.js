318bbe6af2ca608d7937d27f70b8bfed4fe8700e
anreese@ucsd.edu <vagrant@precise32.(none)>
2014-01-30 18:53:45 +0000

    node.js lab

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-// Example route
-// var user = require('./routes/user');
+var project = require('./routes/project');
 
 var app = express();
 
@@ -36,9 +35,12 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
